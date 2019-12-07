cdd8830e939fc31338bc166e5f3e8f4be1fc20ab
KingAbesh <abeshekwere@gmail.com>
2019-12-07 10:44:06 +0100

    navbar fix

--- src/Pages/about-us/about-us.styles.jsx
+++ src/Pages/about-us/about-us.styles.jsx
@@ -5,9 +5,9 @@ import {Header} from '../../components/header/header.component';
 
 export const AboutUsHeaderWrapper = styled(Header)`
     height: 70vh;
-
-    div {
+/* 
+    div.landingwords {
         text-align: center;
-    }
+    } */
 
 `
