f89aef7edbb2a036e7460109b4745eedc43f6fc4
J-Engineers <ugboguj@yahoo.com>
2019-12-09 02:45:18 +0100

    adding api endpoints

--- v1/route.php
+++ v1/route.php
@@ -14,15 +14,15 @@ define("customer_login", "POST,customer_login,false,all");
 define("delete_qualification", "POST,delete_qualification,true,admin");
 define("update_qualification", "POST,update_qualification,true,admin");
 define("create_qualification", "POST,create_qualification,true,admin");
-define("qualification", "POST,qualification,true,admin");
+define("qualification", "GET,qualification,true,admin");
 define("delete_location", "POST,delete_location,true,admin");
 define("update_location", "POST,update_location,true,admin");
 define("create_location", "POST,create_location,true,admin");
-define("location", "POST,location,true,admin");
+define("location", "GET,location,true,admin");
 
 //general
 define("get_token", "GET,tokennizer_wocman,false,admin");
-define("get_location", "POST,location,false,all");
-define("get_qualification", "POST,qualification,false,all");
+define("get_location", "GET,location,false,all");
+define("get_qualification", "GET,qualification,false,all");
 
 
