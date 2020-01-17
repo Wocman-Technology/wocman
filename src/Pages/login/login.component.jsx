d3ba1a1d2071159e93229136d88fd8dbf7749a80
KingAbesh <abeshekwere@gmail.com>
2020-01-17 22:33:59 +0100

    log in functionality

--- src/Pages/login/login.component.jsx
+++ src/Pages/login/login.component.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import { Loginwrapper } from "./login.styles"
+import React from "react";
+import { Loginwrapper } from "./login.styles";
+import { Link } from "react-router-dom";
+import NavBarLogo from "../../assets/Logo.svg";
 
 export const Login = () => (
-    <Loginwrapper>
-        
-    </Loginwrapper>
+  <Loginwrapper>
+    <div className="img__div">
+      <Link to="/">
+        <img className="pl-3" src={NavBarLogo} alt="logo" />
+      </Link>
+    </div>
+    <div className="container">
+        <div classname="row">
+            <div className="col-md-6 col-sm-12 text-white text-break">
+                <h2 className="pb-4">Sign Up to start earning by solving real life problems</h2>
+                <small>Wocman lets customers find available artisans and workmen around their location</small>
+            </div>
+            <div className="col-md-6 col-sm-12">
+                <form className="">
+
+                </form>
+            </div>
+        </div>
+    </div>
+  </Loginwrapper>
 );
--- src/Pages/login/login.styles.jsx
+++ src/Pages/login/login.styles.jsx
@@ -1,13 +1,36 @@
 import styled from "styled-components";
 import AuthImage from "../../assets/bg-image.jpg";
 
-export const Loginwrapper = styled.div`
+export const Loginwrapper = styled.body`
     height: 100vh;
     width: auto;
     background-image: url(${AuthImage});
     background-position: center;
-    background-repeat: no-repeat;
     object-fit: contain;
     background-repeat: no-repeat;
     background-size: cover;
+    .img__div {
+        position: absolute;
+        margin: 2rem;
+        cursor: pointer;
+    }
+    .container {
+        margin-top: 15rem;
+        div:nth-child(1) {
+            div:nth-child(1) {
+        h2 {
+            font-style: normal;
+            font-weight: bold;
+            font-size: 36px;
+            line-height: 48px;
+        }
+        small {
+            font-style: normal;
+            font-weight: normal;
+            font-size: 17px;
+            line-height: 31px;
+        }
+            }
+    }
+    }
 `;
