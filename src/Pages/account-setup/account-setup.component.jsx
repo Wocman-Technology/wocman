1f5b0e688479b725fe87be43f9ac3f72da1a199c
KingAbesh <abeshekwere@gmail.com>
2020-01-23 07:53:35 +0100

    account setup

--- src/Pages/account-setup/account-setup.component.jsx
+++ src/Pages/account-setup/account-setup.component.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
-import { NavBar } from '../../components/navbar/navbar.component';
-import { Footer } from '../../components/footer/footer.component';
-import { AccountSetUpWrapper } from './account-setup.styles';
+import React from "react";
+import { NavBar } from "../../components/navbar/navbar.component";
+import { Footer } from "../../components/footer/footer.component";
+import {
+  AccountSetUpWrapper,
+  AccountSetUpButton
+} from "./account-setup.styles";
 
 export const AccountSetup = () => {
-    return (
-        <AccountSetUpWrapper>
-            <NavBar/>
-            <Footer/>
-        </AccountSetUpWrapper>
-    )
-}
+  return (
+    <>
+      <NavBar />
+      <AccountSetUpWrapper>
+        <div>
+          <h2>Complete Your Account Setup</h2>
+          <p>blah@gmail.com</p>
+        </div>
+        <div>
+          <form action="">
+            <p>Profile</p>
+            <div className="input-container">
+              <input type="text" name="" id="" />
+              <input type="text" name="" id="" />
+              <input type="text" name="" id="" />
+            </div>
+            <p>Location</p>
+            <div className="input-container">
+              <input type="text" name="" id="" />
+              <input type="text" name="" id="" />
+            </div>
+            <div className="terms">
+              <small>
+                Yes, I understand and agree to the Wocman Terms of service,
+                including the User Agreement and Privacy Policy
+              </small>
+              <input type="checkbox" name="accept" id="" />
+            </div>
+            <AccountSetUpButton type="submit">Submit</AccountSetUpButton>
+          </form>
+        </div>
+      </AccountSetUpWrapper>
+      <Footer />
+    </>
+  );
+};
--- src/Pages/account-setup/account-setup.styles.jsx
+++ src/Pages/account-setup/account-setup.styles.jsx
@@ -1,5 +1,58 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { CustomButton } from "../../components/custom-button/custom-button.component";
 
 export const AccountSetUpWrapper = styled.div`
-    
-`
+  height: 83vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2rem auto;
+  padding: 2rem;
+  max-width: 60%;
+  background: #ffffff;
+  box-shadow: 2px 5px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  div:first-child {
+    text-align: center;
+    p {
+      margin: 1rem 0;
+    }
+  }
+  .input-container {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: space-between;
+    input {
+      display: inline-block;
+      width: 47%;
+      margin: 1.5rem 0;
+      outline: none;
+      background: #ffffff;
+      border: 1px solid rgba(85, 45, 30, 0.4);
+      height: 40px;
+      box-sizing: border-box;
+      border-radius: 5px;
+    }
+  }
+  .terms {
+    width: 60%;
+    margin: auto;
+    input[type="checkbox"] {
+      display: block;
+      color: #552d1e;
+      background: #ffffff;
+      border-radius: 4px;
+      margin: 1rem 0;
+      &:checked {
+        background: #552d1e;
+        color: #ffffff;
+      }
+    }
+  }
+`;
+
+export const AccountSetUpButton = styled(CustomButton)`
+  border-radius: 6px;
+  margin: 1rem auto;
+`;
